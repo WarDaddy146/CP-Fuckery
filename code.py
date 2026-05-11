7b6ad157eacb6b5f0a3f08e1b5a1b34ad7797d65
WarDaddy146 <aliakbarnashit146@gmail.com>
2026-05-11 19:21:19 +0530

    Working LED Chain

--- code.py
+++ code.py
@@ -4,16 +4,25 @@ import time
 
 # Change the 1 to however many LEDs you have!
 # Example: neopixel.NeoPixel(board.GP2, 30, brightness=0.1)
-pixel = neopixel.NeoPixel(board.D2, 1, brightness=0.9)
+pixel = neopixel.NeoPixel(board.P0_06, 5, brightness=0.9)
 
 # pixel[0] lights up the first LED
 # pixel[1] lights up the second LED
 # pixel[2] lights up the third LED... and so on!
 
 while True:
-    print("Turning on the LED")
+    print("Red")
     pixel[0] = (255, 0, 0)
     time.sleep(10)
-    print("Turning off the LED")
-    pixel[0] = (0, 0, 0)
+    print("Green")
+    pixel[1] = (0, 255, 0)
+    time.sleep(1)
+    print("Blue")
+    pixel[2] = (0, 0, 255)
+    time.sleep(1)
+    print("White")
+    pixel[3] = (255, 255, 255)
+    time.sleep(1)
+    print("Off")
+    pixel[4] = (0, 0, 0)
     time.sleep(1)
