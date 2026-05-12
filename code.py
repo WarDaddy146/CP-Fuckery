a7ad42ba65d88c2fb24a3cc16c97489913eb43e0
WarDaddy146 <aliakbarnashit146@gmail.com>
2026-05-12 12:00:30 +0530

    Light up all LEDs

--- code.py
+++ code.py
@@ -9,20 +9,4 @@ pixel = neopixel.NeoPixel(board.P0_06, 5, brightness=0.9)
 # pixel[0] lights up the first LED
 # pixel[1] lights up the second LED
 # pixel[2] lights up the third LED... and so on!
-
-while True:
-    print("Red")
-    pixel[0] = (255, 0, 0)
-    time.sleep(10)
-    print("Green")
-    pixel[1] = (0, 255, 0)
-    time.sleep(1)
-    print("Blue")
-    pixel[2] = (0, 0, 255)
-    time.sleep(1)
-    print("White")
-    pixel[3] = (255, 255, 255)
-    time.sleep(1)
-    print("Off")
-    pixel[4] = (0, 0, 0)
-    time.sleep(1)
+pixel.fill((255, 0, 0))
